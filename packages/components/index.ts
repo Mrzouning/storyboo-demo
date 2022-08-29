256b8f17a0cc476f0ca9c3b8167533949b26fd9f
zou ning <mrzouning@163.com>
2022-08-29 10:30:37 +0800

    feat: update export components

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,3 +1,4 @@
-import hooks from "@storybook/hooks";
-
-console.log(hooks);
+import { Select } from "./stories/Select";
+import { Button } from "./stories/Button";
+import { Header } from "./stories/Header";
+export default { Select, Button, Header };
